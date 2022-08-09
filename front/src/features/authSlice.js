df1fbc283bad4d86d0b1519a01e936d65f13ac3f
Alejandro Wogelman <alehellamn@gmail.com>
2022-08-09 00:41:11 -0300

    Store configured. Installing redux and redux-toolkit

---
+++ front/src/features/authSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = null;
+//user
+
+const authSlice = createSlice({
+  initialState,
+  name: "auth",
+  reducers: {},
+});
+
+export default authSlice.reducer;
