fd30450fece460672870ba0d00a798d2f8a95ccb
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-08-14 20:26:55 -0300

    add findByUsername method in UserRepository
    Updated UserService to check if an email already exists in DB before creating a new User

--- src/main/java/com/nocountry/ecommerce/service/impl/UserService.java
+++ src/main/java/com/nocountry/ecommerce/service/impl/UserService.java
@@ -2,6 +2,7 @@ package com.nocountry.ecommerce.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.nocountry.ecommerce.exception.UserAlreadyExistsException;
 import com.nocountry.ecommerce.mapper.UserMapper;
 import com.nocountry.ecommerce.persistence.model.User;
 import com.nocountry.ecommerce.persistence.repository.UserRepository;
@@ -20,6 +21,10 @@ public class UserService implements IUserService {
 
   @Override
   public UserRegisterResponse create(UserRegisterRequest userRegisterReq) {
+    if (userRepository.findByUsername(userRegisterReq.getEmail()) != null) {
+      throw new UserAlreadyExistsException(
+          "Email address: " + userRegisterReq.getEmail() + " is already being used.");
+    }
     User userEntity = userMapper.toEntity(userRegisterReq);
     userEntity.setSoftDelete(false);
     return userMapper.toUserRegisterResponse(userRepository.save(userEntity));
