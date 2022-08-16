d565dda93b00aacdde459a4d204ceb9345a20b92
brianTahiel <briang.2012@hotmail.com>
2022-08-16 04:31:09 -0300

    home fixes

--- front/src/components/home/Home.js
+++ front/src/components/home/Home.js
@@ -3,7 +3,7 @@ import styles from "../home/Home.module.css";
 import data from "../json/data.json"
 import { Link } from "react-router-dom";
 
-const {cardHome, card, cardImg, cardText} = styles
+const {cardHome, card, cardImg, cardText, cardContainer, text, button} = styles
 
 const Home = () => {
   return (
@@ -12,15 +12,17 @@ const Home = () => {
     {
        data.map((product) => {
         return (
-          <div key={product.id}>
+          <div key={product.id} className={cardContainer}>
               <div className={card}>
                 <div className={cardImg}>
                   <img src={product.url} alt="..."/>
                 </div>
                 <div className={cardText}>
+                  <div className={text}>
                   <h1>{product.title}</h1>
                   <p>{product.description}</p>
-                  <Link to={`/${product.title}`}>SEE MORE</Link>
+                 <Link to={`/${product.title}`}> <button>SEE MORE</button></Link>
+                  </div>
                 </div>
              </div>
           </div>
