152c0ac96569cc7d73816a8374207df0eda7fbcb
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-08-24 00:09:35 -0300

    remove unnecessary method

--- src/main/java/com/nocountry/ecommerce/persistence/repository/CategoryRepository.java
+++ src/main/java/com/nocountry/ecommerce/persistence/repository/CategoryRepository.java
@@ -10,7 +10,5 @@ import com.nocountry.ecommerce.persistence.model.Category;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     
-    List<Category> findBySoftDelete(Boolean softDelete);
-
     List<Category> findBySoftDeleteFalse();
 }
