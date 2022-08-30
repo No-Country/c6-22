f7ff24e5eaba5c16411ba011b1bb49bfa7a2c4a1
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-08-30 01:57:33 -0300

    configuration files for deploy in Heroku

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO `categories`(`category_id`, `description`, `image`, `name`, `soft_delete`) VALUES 
+INSERT INTO public.categories (`category_id`, `description`, `image`, `name`, `soft_delete`) VALUES 
 (1, 'Description of laptops', 'https://nocountry-c6-22.s3.amazonaws.com/laptop.webp', 'Laptops', false),
 (2, 'Description of smartphones', 'https://nocountry-c6-22.s3.amazonaws.com/smartphone.webp', 'Smartphones', false),
 (3, 'Description of tablets', 'https://nocountry-c6-22.s3.amazonaws.com/tablet.webp', 'Tablets', false),
