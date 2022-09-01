50d432abdfd5afa0b1202bf31a8dfe7d5125481b
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-08-31 22:26:39 -0300

    add method to list products by category in ProductRepository

--- src/main/java/com/nocountry/ecommerce/persistence/repository/ProductRepository.java
+++ src/main/java/com/nocountry/ecommerce/persistence/repository/ProductRepository.java
@@ -1,13 +1,15 @@
 package com.nocountry.ecommerce.persistence.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.nocountry.ecommerce.persistence.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
   
   Product findByProductIdAndSoftDeleteFalse(Long productId);
+  
+  List<Product> findByCategoryIdAndSoftDeleteFalse(Long categoryId);
 
 }
