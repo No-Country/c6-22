4bb0c3132f003d5c320ca0860e9f34b5c09f5fe6
ojedajuanc <ojeda.juancarlos02@gmail.com>
2022-08-31 22:44:56 -0300

    minor changes to CategoryController

--- src/main/java/com/nocountry/ecommerce/rest/controller/CategoryController.java
+++ src/main/java/com/nocountry/ecommerce/rest/controller/CategoryController.java
@@ -7,7 +7,6 @@ import com.nocountry.ecommerce.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
